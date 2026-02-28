84d9f95897c7c22dabdf314614beff315e51d376
Gabor Szeleczki <szeleg137@gmail.com>
2026-03-01 00:06:26 +0100

    ok, this works again

--- exponential_model.py
+++ exponential_model.py
@@ -9,31 +9,32 @@ class exponential_model:
         self.inital_significant_gradient_min_percentile = settings["e_inital_significant_gradient_min_percentile"]
         self.loss_function = settings["e_loss_function"]
         self.method = settings["e_method"]
+        self.ftol = settings["e_ftol"]
         self.create_coordinates(img)
         print("Estimating initial values...")
         self.params = self.estimate_initial_values(img, mask)
         self.print_params()
-    
+
     # Calculates the length of the projected gradient vector for each pixel.
     def directional_gradient(self, img, angle, mask):
         dIy, dIx = np.gradient(img)
         grad_dir = np.cos(angle) * dIx + np.sin(angle) * dIy
         return grad_dir[mask]
-    
+
     def create_coordinates(self, img):
         ny, nx = img.shape
         y, x = np.mgrid[0:ny, 0:nx]
-    
+
         self.x = x / (nx - 1)
         self.y = y / (ny - 1)
-    
+
     def calc_diagonal(self, img):
         diagonal_x, diagonal_y = img.shape
         return (diagonal_x ** 2.0 + diagonal_y ** 2.0) ** 0.5
-    
+
     def description(self):
         return "Exponential model in a form of [amplitude * exp((cos(direction) * x + sin(direction) * y) * decay) + constant]."
-    
+
     # Calculates the distance of points from a line which goes through the origin.
     # Returns a 2D array.
     def project_coordinates(self, shape, angle):
@@ -41,38 +42,38 @@ class exponential_model:
         y, x = np.indices((height, width))
         projected_coordinates = np.cos(angle) * x + np.sin(angle) * y
         return projected_coordinates
-    
+
     def estimate_decay_and_offset(self, projected_vals, gradient, img_flattened):
         valid_values_mask = np.isfinite(projected_vals) & np.isfinite(gradient) & np.isfinite(img_flattened)
         valid_gradient = gradient[valid_values_mask]
         valid_values = img_flattened[valid_values_mask]
-    
+
         valid_gradient_absolute = np.abs(valid_gradient)
         valid_significant_gradient = valid_gradient_absolute > np.percentile(valid_gradient_absolute, self.inital_significant_gradient_min_percentile)
-    
+
         valid_gradient = valid_gradient[valid_significant_gradient]
         valid_values = valid_values[valid_significant_gradient]
-    
+
         tmp = np.vstack([valid_values, np.ones_like(valid_values)]).T
         decay, intercept = np.linalg.lstsq(tmp, valid_gradient, rcond=None)[0]
-    
+
         offset = -intercept / decay
-    
+
         return decay, offset
-    
+
     def estimate_initial_values(self, img, mask):
         direction = self.estimate_direction(img, mask)
-    
+
         projected_vals = self.project_coordinates(img.shape, direction)[mask]
         img_vals = img[mask]
-    
+
         gradient = self.directional_gradient(img, direction, mask)
         decay, offset = self.estimate_decay_and_offset(projected_vals, gradient, img_vals)
-        
+
         return self.estimate_amplitude(img, mask, direction, decay, offset, self.initial_amplitude_clip_percentiles), decay * self.calc_diagonal(img), direction, offset
 
     def generate_background(self, params = None):
-        local_params = self.params if (params is None) else params 
+        local_params = self.params if (params is None) else params
         coefficient, decay, direction, offset = local_params
         proj = np.cos(direction) * self.x + np.sin(direction) * self.y
         return coefficient * np.exp(decay * proj) + offset
@@ -90,50 +91,49 @@ class exponential_model:
         (a, b, c), *_ = np.linalg.lstsq(X, z, rcond=None)
 
         return np.arctan2(b, a)
-        
+
     def residuals(self, params, img, mask):
         background = self.generate_background(params)
         return (img - background)[mask].ravel()
-    
+
     def estimate_amplitude(self, img, mask, angle, decay, offset, clip_percentiles):
         height, width = img.shape
         y, x = np.mgrid[:height, :width]
-    
+
         projected_coordinates = np.cos(angle) * x + np.sin(angle) * y
         exponentials = np.exp(decay * projected_coordinates)
-    
+
         masked_img = img[mask]
         masked_exponentials = exponentials[mask]
-    
+
         valid_mask = np.isfinite(masked_exponentials) & (masked_exponentials > 0)
         masked_img = masked_img[valid_mask]
         masked_exponentials = masked_exponentials[valid_mask]
-    
+
         ratio = (masked_img - offset) / masked_exponentials
         ratio = ratio[np.isfinite(ratio)]
-        
+
         lo, hi = np.percentile(ratio, self.initial_amplitude_clip_percentiles)
         ratio = ratio[(ratio >= lo) & (ratio <= hi)]
         return np.median(ratio)
-    
+
     def set_params(self, params):
         self.params = params
-        
+
     def print_params(self):
         amplitude, decay, angle, offset = self.params
         print(f"amplitude  = {amplitude}")
         print(f"decay      = {decay}")
         print(f"angle      = {angle} rad {angle * 180.0 / np.pi} deg")
         print(f"offset     = {offset}")
-    
+
     def print_pixelmath_expression(self, img, region_of_interest):
         amplitude, decay, direction, constant = self.params
         cos_angle = np.cos(direction)
         sin_angle = np.sin(direction)
         print(f"$T - {amplitude} * exp({decay / self.calc_diagonal(img)} * ({cos_angle} * x() + {sin_angle} * y())) - {constant} + {np.median(img[region_of_interest])}")
         # $T - 0.003408024138224537 * exp(-0.4752065977993465 * x() + -0.05742004767700772 * y()) - 0.001582727585407239 + 0.004
-    
+
     def fit_params(self, img, region_of_interest):
-# TMP42
-        result = least_squares(self.residuals, self.params, args = (img, region_of_interest), method = self.method, loss = self.loss_function, verbose = 2, f_scale = np.std(img[region_of_interest]))
+        result = least_squares(self.residuals, self.params, args = (img, region_of_interest), method = self.method, loss = self.loss_function, verbose = 2, ftol = self.ftol, f_scale = np.std(img[region_of_interest]))
         return result.cost, result.x
--- imfbr.py
+++ imfbr.py
@@ -12,10 +12,11 @@ import sys
 from pathlib import Path
 import re
 import exponential_model as em
+from pathlib import Path
 
 class imfbr:
     ALL_PARAMETERS = {
-        "input_path": "",
+        "input_paths": "",
         "dark_absolute_threshold": 0.0001,
         "background_percentile": 10.0,
         "discarded_edge_size": 50,
@@ -24,14 +25,20 @@ class imfbr:
         "min_cost_change" : 2e-6,
         "model_type" : "e",
         "print_pixelmath_expression" : True,
-        
+        "show_debug_pictures" : False,
+
         "e_inital_amplitude_clip_percentile_min" : 40,
         "e_inital_amplitude_clip_percentile_max" : 60,
         "e_inital_significant_gradient_min_percentile" : 5.0,
         "e_loss_function" : "linear",
-        "e_method" : "trf"
+        "e_method" : "trf",
+        "e_ftol" : None
     }
-    
+
+    EXPLICIT_TYPES = {
+        "e_ftol" : float
+    }
+
     def load_config(self):
         config_path = Path("config.ini")
         config = configparser.ConfigParser()
@@ -40,11 +47,11 @@ class imfbr:
             return config["settings"]
         print("Config file [" + str(config_path) + "] not found.")
         return {}
-    
+
     def load_settings(self):
         parser = argparse.ArgumentParser(description="IMFBR - Iterative Model Fitting Background Remover")
-    
-        parser.add_argument("-i", "--input_path", type=str, help="Input file path (mandatory).")
+
+        parser.add_argument("-i", "--input_paths", type=str, help="Input file paths, separated by |. If omitted, all *.fit images in the current directory will be used.")
         parser.add_argument("-m", "--model_type", type=str, help="Background model:\n  e: Exponential model in a form of [amplitude * exp((cos(direction) * x + sin(direction) * y) * decay) + constant]\n  p[n]: Polynomial with order n")
         parser.add_argument("-bp", "--background_percentile", type=float, help="The percentile of the area which can be considered as background.")
         parser.add_argument("--dark_absolute_threshold", type=float, help="Any pixel value below this will be masked out.")
@@ -53,39 +60,41 @@ class imfbr:
         parser.add_argument("--mask_structure_growth", type=int, help="The unmasked area growth in pixels.")
         parser.add_argument("--min_cost_change", type=float, help="The algorithm terminates if the cost decreases by less than then this amount.")
         parser.add_argument("--print_pixelmath_expression", type=bool, help="Print the pixelmath expression at the end which correct the background.")
-        
+        parser.add_argument("--show_debug_pictures", type=bool, help="Show the current background mask.")
+
         parser.add_argument("--e_inital_amplitude_clip_percentile_min", type=float, help="[exponential model] Min clipping percentile to initially estimate the amplitude.")
         parser.add_argument("--e_inital_amplitude_clip_percentile_max", type=float, help="[exponential model] Max clipping percentile to initially estimate the amplitude.")
         parser.add_argument("--e_inital_significant_gradient_min_percentile", type=float, help="[exponential model] When calculating the gradient, values below this will be not used.")
         parser.add_argument("--e_loss_function", type=str, help="[exponential model] The loss function - see scipy documentation of least squares for possible values.")
         parser.add_argument("--e_method", type=str, help="[exponential model] The fitting method - see scipy documentation of least squares for possible values.")
-    
+        parser.add_argument("--e_ftol", type=float, help="[exponential model] Tolerance for termination by the change of the cost function - see scipy documentation for more info.")
+
         args = parser.parse_args()
-    
+
         config = self.load_config()
         if len(config) == 0:
             exit()
-    
+
         self.settings = {}
-    
+
         for key, default in self.ALL_PARAMETERS.items():
             cli_value = getattr(args, key)
             cfg_value = config.get(key)
-    
+
             if cli_value is not None:
                 self.settings[key] = cli_value
             elif cfg_value is not None:
-                self.settings[key] = type(default)(cfg_value)
-            else:
+                self.settings[key] = self.EXPLICIT_TYPES[key](cfg_value) if key in self.EXPLICIT_TYPES else type(default)(cfg_value)
+            elif key != 'input_paths':
                 self.settings[key] = default
                 print(f"[INFO] Using default for {key}: {default}")
-    
+
         print("Run parameters:")
         for k, v in self.settings.items():
             print(f"  {k} = {v}")
-        
+
         self.model_type = self.settings["model_type"]
-        self.input_path = self.settings["input_path"]
+        self.input_paths = self.settings["input_paths"] if "input_paths" in self.settings else None
         self.dark_absolute_threshold = self.settings["dark_absolute_threshold"]
         self.background_percentile = self.settings["background_percentile"]
         self.discarded_edge_size = self.settings["discarded_edge_size"]
@@ -93,38 +102,39 @@ class imfbr:
         self.mask_structure_growth = self.settings["mask_structure_growth"]
         self.min_cost_change = self.settings["min_cost_change"]
         self.print_pixelmath_expression = self.settings["print_pixelmath_expression"]
-    
-    def load_fits_image(self, filename = None):
-        if filename is None:
-            filename = self.input_path
+        self.show_debug_pictures = self.settings["show_debug_pictures"]
+
+    def load_fits_image(self, filename):
         with fits.open(filename) as hdul:
             data = hdul[0].data
-    
+
         return np.asarray(data, dtype=np.float64)
-    
+
     def create_absolute_dark_mask(self, img, threshold):
         return img > threshold
-    
+
     def create_model_dark_mask(self, background):
         if background is None:
             background = np.zeros_like(self.img)
-    
+
         img_corr = np.full_like(self.img, np.nan)
         img_corr[self.grown_absolute_dark_mask] = self.img[self.grown_absolute_dark_mask] - background[self.grown_absolute_dark_mask]
-    
+
         values = img_corr[self.grown_absolute_dark_mask]
         threshold = np.percentile(values, self.background_percentile)
-    
+
         intermediate = ((img_corr <= threshold) & self.grown_absolute_dark_mask)
-        intermediate = binary_erosion(intermediate, iterations = self.mask_min_structure_size)
-        intermediate = binary_dilation(intermediate, iterations = self.mask_min_structure_size + self.mask_structure_growth)
-        
+        if self.mask_min_structure_size > 0:
+            intermediate = binary_erosion(intermediate, iterations = self.mask_min_structure_size)
+        if self.mask_min_structure_size + self.mask_structure_growth > 0:
+            intermediate = binary_dilation(intermediate, iterations = self.mask_min_structure_size + self.mask_structure_growth)
+
         return intermediate & self.grown_absolute_dark_mask
-            
+
     def calc_diagonal(self):
         diagonal_x, diagonal_y = self.img.shape
         return (diagonal_x ** 2.0 + diagonal_y ** 2.0) ** 0.5
-    
+
     def show_image(self, img, title):
         plt.figure(figsize=(6, 6))
         plt.imshow(img, cmap="gray", origin="lower")
@@ -132,22 +142,40 @@ class imfbr:
         plt.title(title)
         plt.tight_layout()
         plt.show()
-    
+
     def run(self):
+        plt.ion()
         self.load_settings()
-        
+        if not self.input_paths:
+            script_dir = Path(__file__).resolve().parent
+            files = list(script_dir.glob("*.fit"))
+            if len(files) < 1:
+                print("No input files were provided and no *.fits file were found in the current directory.")
+                exit(0)
+            else:
+                for input_path in files:
+                    self.fit_background(str(input_path))
+
+    def fit_background(self, input_path):
         print("")
-        print("Loading image: [" + self.input_path + "]")
-        self.img = self.load_fits_image()
+        print("Loading image: [" + input_path + "]")
+        self.img = self.load_fits_image(input_path)
         print("Image loaded.")
-        
+
         print("Dimensions: " + str(self.img.shape))
         print("Sample type: " + str(self.img.dtype))
-        
+
         print("")
         print("Creating absolute dark mask...")
         self.absolute_dark_mask = self.create_absolute_dark_mask(self.img, self.dark_absolute_threshold)
-        
+        if self.show_debug_pictures:
+            figure, axes = plt.subplots()
+            mask_displayed_image = axes.imshow(self.absolute_dark_mask, cmap='gray', vmin=0, vmax=1)
+            axes.set_title("Initial absolute mask")
+            figure.canvas.manager.set_window_title("IMFBR debug window")
+            figure.canvas.draw()          # render now
+            figure.canvas.flush_events()  # process GUI events
+
         total_pixels = self.absolute_dark_mask.size
         total_pixels_mp = int(total_pixels / 1000000)
         rejected_pixels = (~self.absolute_dark_mask).sum()
@@ -156,13 +184,13 @@ class imfbr:
         kept_pixels_mp = int(kept_pixels / 1000000)
         rejected_pct = 100.0 * rejected_pixels / total_pixels
         kept_pct = 100.0 * kept_pixels / total_pixels
-        
+
         print(f"Total pixels: {total_pixels_mp}MP")
         print(f"Kept pixels: {kept_pixels_mp}MP ({kept_pct:.2f}%)")
         print(f"Rejected pixels: {rejected_pixels_mp}MP ({rejected_pct:.2f}%)")
-        
+
         self.grown_absolute_dark_mask = binary_erosion(self.absolute_dark_mask, iterations = self.discarded_edge_size)
-        
+
         self.model = None
         if self.model_type == "e":
             print("Using exponential model.")
@@ -173,23 +201,28 @@ class imfbr:
         else:
             print("Unknown model: " + self.model_type)
             exit()
-        
+
         union_region_of_interest = np.zeros_like(self.img, dtype=bool)
         region_of_interest = None
         last_cost = float("inf")
         improved = True
-        
+
         while improved:
             print("")
             print("***************************")
             print("Fitting background model...")
-        
+
             print("Calculating region of interest...")
             region_of_interest = self.create_model_dark_mask(None if (region_of_interest is None) else self.model.generate_background())
-            
+            if self.show_debug_pictures:
+                mask_displayed_image.set_data(region_of_interest)
+                axes.set_title("Current mask")
+                figure.canvas.draw_idle()     # schedule redraw
+                figure.canvas.flush_events()  # process events immediately
+
             print(f"Median in region of interest: {np.median(self.img[region_of_interest])}")
-            
-            new_pixels = region_of_interest & ~union_region_of_interest            
+
+            new_pixels = region_of_interest & ~union_region_of_interest
             if not new_pixels.any():
                 print("Stopping as background mask doesn't have new pixels.")
                 improved = False
@@ -197,10 +230,10 @@ class imfbr:
             else:
                 pct_total = new_pixels.sum() / region_of_interest.sum() * 100
                 print(f"Mask change: {pct_total:.2f}% of image")
-            
+
             new_cost, new_params = self.model.fit_params(self.img, region_of_interest)
             print(f"New cost: {new_cost} Last cost: {last_cost}")
-            
+
             const_improvement = last_cost - new_cost
             improved = const_improvement > 0.0
             if improved:
@@ -209,16 +242,21 @@ class imfbr:
                 print("New parameters:")
                 self.model.print_params()
                 union_region_of_interest |= region_of_interest
-            
+
             if const_improvement < self.min_cost_change:
                 print("Stopping as cost improvement is less than the threshold.")
                 improved = False
-        
+
         print("Final parameters:")
         self.model.print_params()
         if self.print_pixelmath_expression:
             print("Pixelmath expression:")
             self.model.print_pixelmath_expression(self.img, region_of_interest)
+
+        if self.show_debug_pictures:
+            print("Close the debug window to finish the script.")
+            plt.ioff()        # turn interactive OFF
+            plt.show()        # now blocks until user closes window
 
 instance = imfbr()
 instance.run()
